2bbaba31072edd78f96e449213773ee440ab8cdd
Edward484 <selea.eduard@gmail.com>
2021-09-22 20:48:00 +0300

    made weather window reopenable still the weather info in the main notes window doesn't get updated

--- View/Weather.xaml.cs
+++ View/Weather.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,11 @@ namespace NoteApplication.View
         public Weather()
         {
             InitializeComponent();
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = true;  // cancels the window close    
+            this.Hide();      // Programmatically hides the window
         }
 
     }
